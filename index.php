b48416323714743e058c6766e5effb6241bfbcb9
TimDer <timderksen97@icloud.com>
2020-01-25 16:44:31 +0100

    Use TD_dbExport plugin

--- index.php
+++ index.php
@@ -3,3 +3,10 @@
 require __DIR__ . "/autoload.php";
 require __DIR__ . "/config.php";
 require config_dir::ROUTES("/routes.php");
+
+
+// Export database but not on AJAX request
+if( !isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) {
+    $export_database = new TD_dbExport(database::$conn);
+    $export_database->export_to_file("build-cms");
+}
