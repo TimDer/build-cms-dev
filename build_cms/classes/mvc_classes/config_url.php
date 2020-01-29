ada724ef7f5ab8d637d18d3af1cc3511c0a069f9
TimDer <timderksen97@icloud.com>
2020-01-29 15:20:19 +0100

    Move to /build_cms directory

--- build_cms/classes/mvc_classes/config_url.php
+++ build_cms/classes/mvc_classes/config_url.php
@@ -82,10 +82,10 @@ class config_url {
 
     /* ============================== view ============================== */
         public static function VIEW_TEMP($url = "") {
-            return self::BASE("/view/template" . $url);
+            return self::BASE("/build_cms/view/template" . $url);
         }
         public static function VIEW($url = "") {
-            return self::BASE("/view" . $url);
+            return self::BASE("/build_cms/view" . $url);
         }
     /* ============================== /view ============================== */
 }
--- view/admin/settings/menus/head.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="<?php echo config_url::BASE("/view/admin/settings/menus/css/main.css"); ?>">
-<link rel="stylesheet" href="<?php echo config_url::BASE("/view/admin/settings/menus/css/tabs.css"); ?>">
-<link rel="stylesheet" href="<?php echo config_url::BASE("/view/admin/settings/menus/css/form.css"); ?>">
