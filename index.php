c0d2d8adfc38133f8dfb2cf96df8f77cd211be2a
TimDer <timderksen97@icloud.com>
2020-01-29 18:30:05 +0100

    Move to /build_cms directory

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 
-require __DIR__ . "/build_cms/autoload.php";
-require __DIR__ . "/build_cms/config.php";
-require config_dir::ROUTES("/routes.php");
-
+require __DIR__ . "/build_cms/start.php";
 
 // Export database but not on AJAX request
 if( !isset($_SERVER['HTTP_X_REQUESTED_WITH']) ) {
+    require __DIR__ . "/TD_dbExport/TD_dbExport.php";
     $export_database = new TD_dbExport(database::$conn);
     $export_database->export_to_file("build-cms");
 }
