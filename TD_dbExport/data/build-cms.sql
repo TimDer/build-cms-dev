578695fe53bee8b87aa5ab34be265d961530928d
TimDer <timderksen97@icloud.com>
2020-01-29 12:47:22 +0100

    Database update

--- TD_dbExport/data/build-cms.sql
+++ TD_dbExport/data/build-cms.sql
@@ -46,7 +46,7 @@ CREATE TABLE `page` (
   `category_text` varchar(6000) NOT NULL,
   `time_stamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 ALTER TABLE `page` ADD PRIMARY KEY (`id`);
 
 -- page_blocks
@@ -58,7 +58,7 @@ CREATE TABLE `page_blocks` (
   `building_blocks_area` varchar(6000) NOT NULL,
   `the_order` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4;
 ALTER TABLE `page_blocks` ADD PRIMARY KEY (`id`);
 
 -- page_cc_block
@@ -69,7 +69,7 @@ CREATE TABLE `page_cc_block` (
   `column_id` int(2) NOT NULL,
   `width` decimal(65,30) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 ALTER TABLE `page_cc_block` ADD PRIMARY KEY (`id`);
 
 -- page_img_block
@@ -84,7 +84,7 @@ CREATE TABLE `page_img_block` (
   `img_height` int(11) NOT NULL,
   `image_align` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 ALTER TABLE `page_img_block` ADD PRIMARY KEY (`id`);
 
 -- page_plain_text
@@ -95,7 +95,7 @@ CREATE TABLE `page_plain_text` (
   `block_id` bigint(20) NOT NULL,
   `data` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 ALTER TABLE `page_plain_text` ADD PRIMARY KEY (`id`);
 
 -- page_sub_cat
@@ -108,7 +108,7 @@ CREATE TABLE `page_sub_cat` (
   `the_limit` bigint(20) NOT NULL,
   `sort` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 ALTER TABLE `page_sub_cat` ADD PRIMARY KEY (`id`);
 
 -- page_wysiwyg
@@ -119,7 +119,7 @@ CREATE TABLE `page_wysiwyg` (
   `block_id` bigint(20) NOT NULL,
   `data` longtext NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 ALTER TABLE `page_wysiwyg` ADD PRIMARY KEY (`id`);
 
 -- settings
