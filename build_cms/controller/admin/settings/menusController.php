f34497d799681ce7bbf9a3088c448b5da0c90dea
TimDer <timderksen97@icloud.com>
2020-02-01 17:10:28 +0100

    Login check to router

--- build_cms/controller/admin/settings/menusController.php
+++ build_cms/controller/admin/settings/menusController.php
@@ -2,16 +2,11 @@
 
 class menusController extends controller {
     public static function get_menus() {
-        user_session::check_session("user_id", function () {
-            user_session::check_session_permission("admin", function () {
-                self::set_head("/admin/settings/menus/head.php");
-                self::set_footer("/admin/settings/menus/footer.php");
+        self::set_head("/admin/settings/menus/head.php");
+        self::set_footer("/admin/settings/menus/footer.php");
 
-
-                self::getView("/admin/admin_basics/header.php");
-                self::getView("/admin/settings/menus/menus.php");
-                self::getView("/admin/admin_basics/footer.php");
-            });
-        });
+        self::getView("/admin/admin_basics/header.php");
+        self::getView("/admin/settings/menus/menus.php");
+        self::getView("/admin/admin_basics/footer.php");
     }
 }
