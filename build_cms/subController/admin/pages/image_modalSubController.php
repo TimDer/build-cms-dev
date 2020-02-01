5c11baf39e3807a1670933381d85db2216c3c195
TimDer <timderksen97@icloud.com>
2020-02-01 17:19:16 +0100

    add "image_modal" modal

--- build_cms/subController/admin/pages/image_modalSubController.php
+++ build_cms/subController/admin/pages/image_modalSubController.php
@@ -7,10 +7,10 @@ class image_modalSubController extends controller {
                 database::reset();
                 database::select("SELECT * FROM `media` WHERE `media_type` = 'image'", function ($data) {
                     foreach ($data["fetch_all"] as $value) {
-                        $image_id   = $value["id"];
-                        $image_url  = $value["the_file_name"];
+                        image_modalModal::$image_id   = $value["id"];
+                        image_modalModal::$image_url  = $value["the_file_name"];
 
-                        require config_dir::VIEW("/admin/page/image-modal.php");
+                        self::getView("/admin/page/image-modal.php");
                     }
                 });
             });
