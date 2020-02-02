0932df7c7232d5a4870024c2a3ada0a369d66ce1
TimDer <timderksen97@icloud.com>
2020-02-02 20:48:57 +0100

    edit menus update

--- build_cms/view/admin/settings/menus/edit.php
+++ build_cms/view/admin/settings/menus/edit.php
@@ -1,12 +1,45 @@
 <div class="edit_grid">
     <div class="edit_sidebar">
-        hello
+        <!-- ============================== add page area ============================== -->
+        <div class="add_page_area box">
+            <div class="header">
+                Add page
+            </div>
+            <div class="main">
+                content
+            </div>
+            <div class="footer">
+                <input type="submit" name="add_pages" value="Add pages">
+            </div>
+        </div>
+        <div class="add_page_area box">
+            <div class="header">
+                Custom URL
+            </div>
+            <div class="main">
+                <p>Name:</p>
+                <input type="text" name="add_name_custom_url">
+                <p>Url:</p>
+                <input type="text" name="add_url_custom_url">
+            </div>
+            <div class="footer">
+                <input type="submit" name="add_url" value="Add url">
+            </div>
+        </div>
+        <!-- ============================== /add page area ============================== -->
     </div>
     <div class="main_edit_container">
         <!-- ============================== select a menu ============================== -->
+        <select name="menu_name">
+            <option value="0" disabled selected>Select a menu</option>
+            <option value="1">Root</option>
+            <option value="2">Main</option>
+            <option value="3">Footer</option>
+        </select>
         <!-- ============================== /select a menu ============================== -->
 
         <!-- ============================== Menu name ============================== -->
+        <input type="text" name="rename_menu_name">
         <!-- ============================== /Menu name ============================== -->
 
         <!-- ============================== edit menu area ============================== -->
