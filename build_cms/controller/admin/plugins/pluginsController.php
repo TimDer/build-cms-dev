ede6d223569569cb9e2568e2c072cf5ca0a0ec8b
TimDer <timderksen97@icloud.com>
2020-02-12 21:12:50 +0100

    List plugins

--- build_cms/controller/admin/plugins/pluginsController.php
+++ build_cms/controller/admin/plugins/pluginsController.php
@@ -2,6 +2,8 @@
 
 class pluginsController extends controller {
     public static function get_plugins_view() {
+        pluginsModal::$plugins_array = database::select("SELECT `description`, `name` FROM `plugins`");
+
         self::set_head("/admin/plugins/head.php");
         self::getAdminTemplateView("/admin/plugins/plugins.php");
     }
---
+++ build_cms/modal/admin/pluginsModal.php
@@ -0,0 +1,12 @@
+<?php
+
+class pluginsModal {
+    public static $plugins_array = array(
+        array(
+            "pluginID"          => "082dce0a2fba5d2a4d067648e1a06fabe3d9bd42",
+            "name"              => "No plugin",
+            "directory_name"    => "no_plugin",
+            "description"       => "There is no plugin installed"
+        )
+    );
+}
