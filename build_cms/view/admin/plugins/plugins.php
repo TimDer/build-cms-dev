3f930d3bec35461c4ccb32813154954cc5a4e3b9
TimDer <timderksen97@icloud.com>
2020-02-12 21:12:19 +0100

    Create plugin front

--- build_cms/view/admin/plugins/plugins.php
+++ build_cms/view/admin/plugins/plugins.php
@@ -1,6 +1,24 @@
-<table>
-    <tbody>
-        <th>Plugin name</th>
-        <th>Plugin description</th>
-    </tbody>
-</table>
+<div class="plugins_header">
+    <h1>Plugins</h1>
+</div>
+
+<div class="plugins_main_container">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Plugin name:</th>
+                <th>Plugin description:</th>
+                <th>Action:</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach (pluginsModal::$plugins_array AS $plugins) { ?>
+                <tr>
+                    <td><?php echo $plugins["name"] ?></td>
+                    <td><?php echo $plugins["description"] ?></td>
+                    <td>Delete</td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+</div>
