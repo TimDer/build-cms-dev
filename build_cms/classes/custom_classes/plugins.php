790bf9c547c83b1d72d2a04105403ec09e42d9ea
TimDer <timderksen97@icloud.com>
2020-02-13 21:19:33 +0100

    Create plugin

--- build_cms/classes/custom_classes/plugins.php
+++ build_cms/classes/custom_classes/plugins.php
@@ -1,5 +1,51 @@
 <?php
 
 class plugins {
-    
+    private static function create_file($dir_file) {
+        $create = fopen($dir_file, "w");
+        fclose($create);
+    }
+
+    public static function create_plugin_folder($plugin_dir) {
+        if ( !is_dir( config_dir::BASE("/plugins/" . $plugin_dir) ) ) {
+            $plugin_base_dir = config_dir::BASE("/plugins/" . $plugin_dir);
+
+            // create plugin dir
+            mkdir($plugin_base_dir);
+
+            /* ============================== create folders ============================== */
+            // create classes folder
+            mkdir($plugin_base_dir . "/classes");
+            // create controller folder
+            mkdir($plugin_base_dir . "/controller");
+            // create modal folder
+            mkdir($plugin_base_dir . "/modal");
+            // create view folder
+            mkdir($plugin_base_dir . "/view");
+            // create view/admin folder
+            mkdir($plugin_base_dir . "/view/admin");
+            // create www-root folder
+            mkdir($plugin_base_dir . "/www-root");
+            // create www-root/admin folder
+            mkdir($plugin_base_dir . "/www-root/admin");
+            /* ============================== /create folders ============================== */
+
+            /* ============================== create files ============================== */
+            // create classes/pluginClass_{plugin_dir}.php
+            self::create_file($plugin_base_dir . "/classes/pluginClass_" . $plugin_dir . ".php");
+            // create controller/{plugin_dir}_pluginController.php
+            self::create_file($plugin_base_dir . "/controller/" . $plugin_dir . "_pluginController.php");
+            // create modal/{plugin_dir}_pluginModal.php
+            self::create_file($plugin_base_dir . "/modal/" . $plugin_dir . "_pluginModal.php");
+
+            // create view/admin/menu.php
+            self::create_file($plugin_base_dir . "/view/admin/menu.php");
+            // create www-root/admin/main.css
+            self::create_file($plugin_base_dir . "/www-root/admin/main.css");
+
+            // create routes.php
+            self::create_file($plugin_base_dir . "/routes.php");
+            /* ============================== /create files ============================== */
+        }
+    }
 }
--- build_cms/controller/admin/plugins/createPluginController.php
+++ build_cms/controller/admin/plugins/createPluginController.php
@@ -1,10 +1,36 @@
 <?php
 
 class createPluginController extends controller {
-    public static function get_create_plugins_view() {
+    public static function get_view() {
         pluginsModal::$plugins_array = database::select("SELECT `description`, `name` FROM `plugins`");
 
         self::set_head("/admin/createPlugin/head.php");
+        self::set_footer("/admin/createPlugin/footer.php");
         self::getAdminTemplateView("/admin/createPlugin/create.php");
+    }
+
+    public static function submit() {
+        if ( isset(user_url::$post_var["plugin_name"]) AND isset(user_url::$post_var["directory_name"]) and isset(user_url::$post_var["description"]) ) {
+            $plugin_name    = user_url::$post_var["plugin_name"];
+            $directory_name = user_url::$post_var["directory_name"];
+            $description    = user_url::$post_var["description"];
+
+            if ( is_dir( config_dir::BASE("/plugins/" . $directory_name) ) ) {
+                echo "Plugin already exists";
+            }
+            else {
+                // -------------------------------------------
+                // Plugin does not exist ( create the plugin )
+                // -------------------------------------------
+                plugins::create_plugin_folder($directory_name);
+                database::query("INSERT INTO `plugins` (`name`,
+                                                        `directory_name`,
+                                                        `description`)
+                                                VALUES ('$plugin_name',
+                                                        '$directory_name',
+                                                        '$description')");
+                echo "The plugin has been created";
+            }
+        }
     }
 }
