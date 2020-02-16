af3d5c5641c757b700daf82d64f58e435963ae69
TimDer <timderksen97@icloud.com>
2020-02-16 20:26:16 +0100

    Routes load files login fix

--- build_cms/routes/load_files.php
+++ build_cms/routes/load_files.php
@@ -2,8 +2,8 @@
 
 routes::set("/admin_files/plugins", function () {
     load_filesController::load_file("/www-root/admin/plugins", "select a file (404)");
-});
+}, "user_id", "admin");
 
 routes::set("/view", function () {
     load_filesController::load_file("/www-root", "select a file (404)");
-});
+}, "user_id", "user");
