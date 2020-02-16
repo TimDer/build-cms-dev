8311e154d46b5939d4928086b28f9fc301ebca36
TimDer <timderksen97@icloud.com>
2020-02-16 20:25:21 +0100

    Delete plugin

--- build_cms/controller/admin/plugins/pluginsController.php
+++ build_cms/controller/admin/plugins/pluginsController.php
@@ -2,9 +2,18 @@
 
 class pluginsController extends controller {
     public static function get_plugins_view() {
-        pluginsModal::$plugins_array = database::select("SELECT `description`, `name` FROM `plugins`");
+        pluginsModal::$plugins_array = database::select("SELECT * FROM `plugins` ORDER BY `name`");
 
         self::set_head("/admin/plugins/head.php");
+        self::set_footer("/admin/plugins/footer.php");
         self::getAdminTemplateView("/admin/plugins/plugins.php");
+    }
+
+    public static function delete_plugin() {
+        $id = user_url::$new_uri[0];
+        
+        $sql = database::select("SELECT `directory_name` FROM `plugins` WHERE `pluginID`='$id'");
+        database::query("DELETE FROM `plugins` WHERE `pluginID`='$id'");
+        config_dir::deleteDirectory("/plugins/" . $sql[0]["directory_name"]);
     }
 }
--- build_cms/www-root/admin/plugins/main.js
+++ build_cms/www-root/admin/plugins/main.js
@@ -8,3 +8,23 @@ $('#no_space_allowed').on('keypress', function(e) {
         return false;
     }
 });
+
+$(".delete_btn").click(function () {
+    var delete_btn = $(this);
+    var id = delete_btn.attr("delete_id");
+
+    if (warning_message("Solve this sum in order to delete this plugin", "Are you sure you want to delete this plugin")) {
+        return;
+    }
+
+    var base_url = $("#build_cms_base_url").attr("base_url");
+    $.ajax({
+        url: base_url + "/admin/plugins/delete/" + id,
+        success: function () {
+            delete_btn.parent().parent().remove();
+            setTimeout(function () {
+                alert("The plugin has been successfully deleted");
+            }, 50);
+        }
+    });
+});
