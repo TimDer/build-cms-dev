290884e73edc5e0d50d6af73328562a8f012ffad
TimDer <timderksen97@icloud.com>
2020-02-17 21:14:16 +0100

    Dashboard update

--- build_cms/view/admin/dashboard/dashboard.php
+++ build_cms/view/admin/dashboard/dashboard.php
@@ -1,1 +1,13 @@
-<p>dit is een test via dashboard.php</p>
+<div class="main_header">
+    <h1>Thanks for using Build-CMS</h1>
+</div>
+
+
+<div class="main_container">
+    <div class="sidebar">
+        <img src="<?php echo config_url::BASE("/admin_files/dashboard/default_user_icon.png"); ?>" alt="">
+    </div>
+    <div class="main_content">
+        main content
+    </div>
+</div>
