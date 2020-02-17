3344ee106bb63679c8398d6f3a78421520e246f4
TimDer <timderksen97@icloud.com>
2020-02-17 21:13:26 +0100

    dashboard files rout

--- build_cms/routes/load_files.php
+++ build_cms/routes/load_files.php
@@ -4,6 +4,10 @@ routes::set("/admin_files/plugins", function () {
     load_filesController::load_file("/www-root/admin/plugins", "select a file (404)");
 }, "user_id", "admin");
 
+routes::set("/admin_files/dashboard", function () {
+    load_filesController::load_file("/www-root/admin/dashboard", "select a file (404)");
+}, "user_id", "admin");
+
 routes::set("/view", function () {
     load_filesController::load_file("/www-root", "select a file (404)");
 });
