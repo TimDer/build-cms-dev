74828713767b5c1767afff3651705bc0bc00d7f7
TimDer <timderksen97@icloud.com>
2020-02-20 20:17:35 +0100

    loginController add salt

--- build_cms/controller/admin/loginController.php
+++ build_cms/controller/admin/loginController.php
@@ -36,7 +36,7 @@ class loginController extends controller {
         database::query_num_rows(function () {
             database::fetch_all();
             $user_array     = database::fetch_result()[0];
-            $user_password  = hash("sha512", user_url::$post_var['password']);
+            $user_password  = hash("sha512", user_url::$post_var['password'] . $user_array["password_salt"]);
 
             if ($user_array["password"] === $user_password) {
                 user_session::set_session("user_id", $user_array['id']);
