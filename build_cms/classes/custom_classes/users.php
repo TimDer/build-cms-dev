24a748ca15ae4bc757fb44cff5c23f26eb3ba132
TimDer <timderksen97@icloud.com>
2020-02-23 16:30:26 +0100

    Password salt min max to vars

--- build_cms/classes/custom_classes/users.php
+++ build_cms/classes/custom_classes/users.php
@@ -1,11 +1,11 @@
 <?php
 
 class users {
-    public static function create_password_salt() {
+    public static function create_password_salt($min, $max) {
         $randomString   = "abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789=!@#$%^&*()+:><.,";
         $salt = "";
 
-        $randomLength = rand(1000, 10000);
+        $randomLength = rand($min, $max);
 
         for ($x = 1; $x <= $randomLength; $x++) {
             $salt .= $randomString[rand(0, strlen($randomString)-1)];
