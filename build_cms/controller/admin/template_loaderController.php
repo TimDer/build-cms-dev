2ac93f3e803f8537917a2c30b7afd1c2b69d708c
TimDer <timderksen97@icloud.com>
2020-03-05 13:10:57 +0100

    Load template loader

--- build_cms/controller/admin/template_loaderController.php
+++ build_cms/controller/admin/template_loaderController.php
@@ -7,6 +7,12 @@ class template_loaderController extends controller {
     }
 
     public static function get_template_view() {
-        echo file_get_contents( config_dir::BASE("/../../demo/demo.html") );
+        //echo file_get_contents( config_dir::BASE("/../../demo/demo.html") );
+        $get_template_loader = database::select(
+            "SELECT `plugins`.`directory_name` FROM `plugins`
+            INNER JOIN `settings` ON `plugins`.pluginID = `settings`.`tamplateLoaderID`
+            WHERE `plugins`.`pluginID` = `settings`.`tamplateLoaderID`"
+        )[0];
+        require config_dir::BASE("/plugins/" . $get_template_loader["directory_name"] . "/index.php");
     }
 }
