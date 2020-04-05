d480568a0f89d79c3e87c9029371e65c04b6decf
TimDer <timderksen97@icloud.com>
2020-04-05 12:27:28 +0200

    Delete unnecessary plugins

--- build_cms/plugins/build_cms_page_builder/view/edit/footer.php
+++
@@ -1,1 +0,0 @@
-<script src="<?php echo config_url::BASE("/admin_files/page_builder/edit/js/delete_page.js"); ?>"></script>
--- build_cms/plugins/build_cms_page_builder/view/edit/head.php
+++
@@ -1,1 +0,0 @@
-<link rel="stylesheet" href="<?php echo config_url::BASE("/admin_files/page_builder/edit/css/main.css"); ?>">
