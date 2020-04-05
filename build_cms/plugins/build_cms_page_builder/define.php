2e9c2902ff396e9aab4e3a87c41e33680c80e193
TimDer <timderksen97@icloud.com>
2020-04-05 12:27:28 +0200

    Delete unnecessary plugins

--- build_cms/plugins/build_cms_page_builder/define.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-plugins::set_menu_item("TD_Page_builder", "Pages", "", "author");
-plugins::set_submenu_item("TD_Page_builder", "Add page", "/admin/pages", "author");
-plugins::set_submenu_item("TD_Page_builder", "Edit pages", "/admin/pages/edit-pages", "author");
--- build_cms/plugins/build_cms_page_builder/modal/image_modalModal.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class image_modalModal {
-    public static $image_id     = "";
-    public static $image_url    = "";
-}
