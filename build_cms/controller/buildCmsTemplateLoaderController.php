f25f3d79af202a71f405ee33113eaddf515828ca
TimDer <timderksen97@icloud.com>
2020-04-13 15:00:45 +0200

    Load template functions

--- build_cms/controller/buildCmsTemplateLoaderController.php
+++ build_cms/controller/buildCmsTemplateLoaderController.php
@@ -2,12 +2,12 @@
 
 class buildCmsTemplateLoaderController extends controller {
     public static function get_template() {
-        if (file_exists( templateLoaderFiles::base_template_dir("/" . templateLoaderFiles::$template_file) )) {
-            $functions = config_dir::BASE("/view/templates/" . templateLoaderFiles::$template_dir . "/functions.php ");
+        if (file_exists( templateLoader::base_template_dir("/" . templateLoader::$template_file) )) {
+            $functions = config_dir::BASE("/view/templates/" . templateLoader::$template_dir . "/functions.php");
             if (file_exists($functions)) {
                 require $functions;
             }
-            self::getView("/templates/" . templateLoaderFiles::$template_dir . "/" . templateLoaderFiles::$template_file);
+            self::getView("/templates/" . templateLoader::$template_dir . "/" . templateLoader::$template_file);
         }
         else {
             echo "nope";
