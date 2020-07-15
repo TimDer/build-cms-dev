77fa5ca1f67df60e6e422dce6ea5296f32f0c1e1
TimDer <timderksen97@icloud.com>
2020-07-15 22:11:16 +0200

    Plugin download and installer

--- build_cms/routes/plugins.php
+++ build_cms/routes/plugins.php
@@ -1,15 +1,17 @@
 <?php
 
 // ============================== create a plugin ==============================
-    // Create (html display)
-    routes::set("/admin/plugins/create", function () {
-        createPluginController::get_view();
-    }, "user_id", "admin");
+    users::is_developer(function () {
+        // Create (html display)
+        routes::set("/admin/plugins/create", function () {
+            createPluginController::get_view();
+        }, "user_id", "admin");
 
-    // Create (submit)
-    routes::set("/admin_submit/plugins/create", function () {
-        createPluginController::submit();
-    }, "user_id", "admin");
+        // Create (submit)
+        routes::set("/admin_submit/plugins/create", function () {
+            createPluginController::submit();
+        }, "user_id", "admin");
+    });
 // ============================== /create a plugin ==============================
 
 // ============================== edit plugins ==============================
@@ -17,8 +19,14 @@
         pluginsController::get_plugins_view();
     }, "user_id", "admin");
 
+    users::is_developer(function () {
+        routes::set("/admin_submit/plugins/download/", function () {
+            pluginsController::download_plugin();
+        }, "user_id", "admin");
+    });
+
     routes::set("/admin_submit/plugins/install", function () {
-        //
+        pluginsController::install_plugin();
     }, "user_id", "admin");
 
     routes::set("/admin_submit/plugins/delete", function () {
