f90c63b76aa90dbd93ae4922043684909d8f5b89
TimDer <timderksen97@icloud.com>
2020-07-15 21:05:51 +0200

    Root index update

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 // do not change this line (config_dir::ROOT_DIR())
 $GLOBALS["index_root_dir"] = __DIR__;
 
+// dir to build_cms
+$GLOBALS["dir_to_build_cms"] = __DIR__ . "/build_cms";
+
 // load the CMS
-require __DIR__ . "/build_cms/start.php";
+require $GLOBALS["dir_to_build_cms"] . "/start.php";
 
 // Export database but not on AJAX request
 /*if  (
