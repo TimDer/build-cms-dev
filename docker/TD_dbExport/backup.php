6b5ccdb91b62dc43c6c112b2dc928146360a48a3
TimDer <timderksen97@icloud.com>
2020-08-03 19:19:56 +0200

    DB exporter to docker service

---
+++ docker/TD_dbExport/backup.php
@@ -0,0 +1,16 @@
+<?php
+
+require "/TD_dbExport/library/TD_dbExport.php";
+
+while (true) {
+    sleep(3);
+    $export_database = new TD_dbExport(array(
+        "servername" => "build_cms_database",
+        "username" => "root",
+        "password" => "root",
+        "dbname" => "build-cms"
+    ));
+    
+    $export_database->set_database_dir("/TD_dbExport/data");
+    $export_database->export_to_file("build-cms");
+}
