5fcc8ecfed35d4818dc20e45a89ad5f3d040cbd4
TimDer <timderksen97@icloud.com>
2020-08-03 19:19:56 +0200

    DB exporter to docker service

--- app/index.php
+++ app/index.php
@@ -8,30 +8,3 @@ $GLOBALS["dir_to_build_cms"] = __DIR__ . "/build_cms";
 
 // load the CMS
 require $GLOBALS["dir_to_build_cms"] . "/start.php";
-
-// Export database but not on AJAX request
-/*if  (
-        !isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-        implode("/", user_url::$routes_uri) !== "images" &&
-        implode("/", user_url::$routes_uri) !== "view" &&
-        implode("/", user_url::$routes_uri) !== "admin_files/plugins"
-    )
-{*/
-if  (
-    !isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
-    implode("/", user_url::$routes_uri) !== "images" &&
-    implode("/", user_url::$routes_uri) !== "downloads" &&
-    implode("/", user_url::$routes_uri) !== "view" &&
-    (isset(user_url::$routes_uri[0]) && user_url::$routes_uri[0] !== "admin_files")
-)
-{
-    require __DIR__ . "/TD_dbExport/TD_dbExport.php";
-    $export_database = new TD_dbExport(database::$conn);
-    /*$export_database = new TD_dbExport(array(
-        "servername" => "localhost",
-        "username" => "root",
-        "password" => "root",
-        "dbname" => "build-cms"
-    ));*/
-    $export_database->export_to_file("build-cms");
-}
