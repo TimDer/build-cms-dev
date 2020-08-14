b7f67df7e6d928298439d81a082e42af77db0749
TimDer <timderksen97@icloud.com>
2020-08-14 20:54:23 +0200

    Database update

--- docker/TD_dbExport/data/build-cms.sql
+++ docker/TD_dbExport/data/build-cms.sql
@@ -15,6 +15,7 @@ CREATE TABLE `menu_content` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `the_name` varchar(6000) NOT NULL,
   `the_url` varchar(6000) NOT NULL,
+  `type` varchar(50) NOT NULL,
   `the_order` bigint NOT NULL,
   `parent_id` bigint NOT NULL,
   `menu_name_id` bigint NOT NULL,
