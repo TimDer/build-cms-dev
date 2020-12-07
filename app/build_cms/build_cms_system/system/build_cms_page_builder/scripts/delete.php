908fed94e7dcfd87e7da09b7f975ddc03f9b11f0
TimDer <timderksen97@icloud.com>
2020-12-07 21:10:13 +0100

    Build_cms_page_builder create installer & deleter

---
+++ app/build_cms/build_cms_system/system/build_cms_page_builder/scripts/delete.php
@@ -0,0 +1,15 @@
+<?php
+
+database::query("DROP TABLE page;");
+
+database::query("DROP TABLE page_blocks;");
+
+database::query("DROP TABLE page_cc_block;");
+
+database::query("DROP TABLE page_img_block;");
+
+database::query("DROP TABLE page_plain_text;");
+
+database::query("DROP TABLE page_sub_cat;");
+
+database::query("DROP TABLE page_wysiwyg;");
