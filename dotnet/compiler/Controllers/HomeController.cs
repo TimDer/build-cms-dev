beb7d871bf8665f491e09ff23ea13029ad6712a4
TimDer <timderksen97@icloud.com>
2020-12-17 13:04:08 +0100

    Add dotnet compiler

---
+++ dotnet/compiler/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using compiler.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace compiler.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult IndexPost()
+        {
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Info()
+        {
+            return View();
+        }
+    }
+}
