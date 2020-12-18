72ed85e40b29affc19e71cd87e6ae1a816dd07e3
TimDer <timderksen97@icloud.com>
2020-12-18 14:12:56 +0100

    Create compiler dev environment

---
+++ start-docker.dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# build the compiler
+echo "Build the docker image"
+sudo docker-compose -f docker-compose.dev.yml up
+
+# stop all docker containers
+sudo docker-compose -f docker-compose.dev.yml down -v --rmi all
