92d7574e47441e606afe8c929984974bdc713d58
feminist-dating <feminist.dating.com@gmail.com>
2021-03-10 17:34:36 +0100

    Add Singleton MockPluginDirRepo

--- compiler/Startup.cs
+++ compiler/Startup.cs
@@ -1,3 +1,4 @@
+using compiler.Models.pluginsDir;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,6 +25,7 @@ namespace compiler
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IPluginsDirRepo, MockPluginDirRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
