522f3f2f6b53762b5bbccb96ac06f9ef5ebbb9be
TimDer <timderksen97@icloud.com>
2021-03-10 17:35:01 +0100

    Remove (-v --rmi all)

--- start-docker.dev.sh
+++ start-docker.dev.sh
@@ -5,4 +5,4 @@ echo "Build the docker image"
 sudo docker-compose -f docker-compose.dev.yml up
 
 # stop all docker containers
-sudo docker-compose -f docker-compose.dev.yml down -v --rmi all
+sudo docker-compose -f docker-compose.dev.yml down
