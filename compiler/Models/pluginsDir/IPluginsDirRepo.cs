2916c4d825879cd048a035085ae87c9f29ee510e
TimDer <timderksen97@icloud.com>
2021-03-10 17:32:35 +0100

    Display plugins

---
+++ compiler/Models/pluginsDir/IPluginsDirRepo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace compiler.Models.pluginsDir
+{
+    public interface IPluginsDirRepo
+    {
+        List<PluginsDir> GetPluginDir();
+    }
+}
---
+++ compiler/Models/pluginsDir/MockPluginDirRepo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace compiler.Models.pluginsDir
+{
+    public class MockPluginDirRepo : IPluginsDirRepo
+    {
+        public List<PluginsDir> GetPluginDir()
+        {
+            var pluginDir = "/build_cms/plugins";
+            var systemPluginDir = "/build_cms/system-plugins";
+
+            string[] pluginDirContents = Directory.GetDirectories(pluginDir);
+            string[] systemDirContents = Directory.GetDirectories(systemPluginDir);
+
+            // create list
+            List<PluginsDir> pluginList = new List<PluginsDir>();
+
+            // add the plugin dir to the list
+            foreach (string plugin in pluginDirContents)
+            {
+                var pluginName = new DirectoryInfo(plugin).Name;
+
+                pluginList.Add(new PluginsDir()
+                {
+                    PluginPath = plugin,
+                    PluginName = pluginName,
+                    PluginType = "plugin"
+                });
+            }
+
+            // add the system dir to the list
+            foreach (string plugin in systemDirContents)
+            {
+                var pluginName = new DirectoryInfo(plugin).Name;
+
+                pluginList.Add(new PluginsDir()
+                {
+                    PluginPath = plugin,
+                    PluginName = pluginName,
+                    PluginType = "system"
+                });
+            }
+
+            return pluginList;
+        }
+    }
+}
