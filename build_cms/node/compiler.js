a8625151a81ddfacc92ad1e57479ab02f0ce0c29
TimDer <timderksen97@icloud.com>
2021-04-08 21:08:39 +0200

    Compiler to function

--- build_cms/node/compiler.js
+++ build_cms/node/compiler.js
@@ -1,52 +1,49 @@
-const fs = require("fs")
-const fse = require("fs-extra")
-const path = require("path")
-const delDir = require("rimraf")
-
-// set dirs
-let dir = {
-    sysDir: path.resolve(__dirname + "/../app"),
-    installer: path.resolve(__dirname + "/../php_installer/install.php"),
-    installerDumpDir: path.resolve(__dirname + "/../installer-dump"),
-    installerFile: path.resolve(__dirname + "/../php_installer/install.php"),
-    dbFile: path.resolve(__dirname + "/../docker/TD_dbExport/data/build-cms.json")
-}
-
-new Promise((res, rej) => {
+module.exports = (async () => {
+    const fs = require("fs")
+    const fse = require("fs-extra")
+    const path = require("path")
+    const delDir = require("rimraf")
+    
+    // set dirs
+    let dir = {
+        sysDir: path.resolve(__dirname + "/../app"),
+        installer: path.resolve(__dirname + "/../php_installer/install.php"),
+        installerDumpDir: path.resolve(__dirname + "/../installer-dump"),
+        installerFile: path.resolve(__dirname + "/../php_installer/install.php"),
+        dbFile: path.resolve(__dirname + "/../docker/TD_dbExport/data/build-cms.json")
+    }
+    
+    
     // delete dir if it already exists
     if (fs.existsSync(dir.installerDumpDir)) {
         console.log("Deleting existing directory")
-        res(delDir.sync(dir.installerDumpDir))
+        delDir.sync(dir.installerDumpDir)
     }
-    else {
-        res(true)
-    }
-}).then(result => {
     // create the installer dump dir
     console.log("Creating \"installer-dump\" directory")
-    return fs.mkdirSync(dir.installerDumpDir)
-}).then(result => {
+    fs.mkdirSync(dir.installerDumpDir)
+    
     // create the sys dir
     console.log("Creating \"sys\" directory")
-    return fs.mkdirSync(dir.installerDumpDir + "/sys")
-}).then(result => {
+    fs.mkdirSync(dir.installerDumpDir + "/sys")
+    
     // copy the cms to the sys dir
     console.log("Coping the cms to the \"sys\" directory")
-    return fse.copy(dir.sysDir + "/build_cms", dir.installerDumpDir + "/sys/build_cms")
-}).then(result => {
+    await fse.copy(dir.sysDir + "/build_cms", dir.installerDumpDir + "/sys/build_cms")
+    
     // copy the ".htaccess" file to the sys dir
     console.log("Coping the htaccess to the \"sys\" directory")
-    return fse.copy(dir.sysDir + "/.htaccess", dir.installerDumpDir + "/sys/.htaccess")
-}).then(result => {
+    await fse.copy(dir.sysDir + "/.htaccess", dir.installerDumpDir + "/sys/.htaccess")
+    
     // create the database installer
     console.log("Copying database")
-    return fse.copy(dir.dbFile, dir.installerDumpDir + "/db.json")
-}).then(result => {
+    await fse.copy(dir.dbFile, dir.installerDumpDir + "/db.json")
+    
     // create the installer html
     console.log("Copying install.php")
-    return fse.copy(dir.installerFile, dir.installerDumpDir + "/index.php")
-}).then(result => {
+    await fse.copy(dir.installerFile, dir.installerDumpDir + "/index.php")
+
     // the compiler is done
     console.log("Done")
     process.exit()
-})
+});
