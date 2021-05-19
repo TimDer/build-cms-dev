92bfe98769b7645d45048dbd8a27a46b45129df9
TimDer <timderksen97@icloud.com>
2021-05-19 20:04:19 +0200

    Bug fixes

--- build_cms/plugins/system/build_cms_menus/classes/pluginClass_build_cms_menus.php
+++ build_cms/plugins/system/build_cms_menus/classes/pluginClass_build_cms_menus.php
@@ -4,8 +4,10 @@ class pluginClass_build_cms_menus {
     public static function query_menu_fix($array) {
         $return_array = array();
 
-        foreach ($array AS $value_content) {
-            $return_array[$value_content["parent_id"]][] = $value_content;
+        if (is_array($array)) {
+            foreach ($array AS $value_content) {
+                $return_array[$value_content["parent_id"]][] = $value_content;
+            }
         }
 
         return $return_array;
