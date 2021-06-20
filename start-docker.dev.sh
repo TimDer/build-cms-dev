66114388ecc8087a048ec6fef57db3ed3ca68d96
TimDer <timderksen97@icloud.com>
2021-06-20 20:14:51 +0200

    Delete the dotnet compiler

--- start-docker.dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# build the compiler
-echo "Build the docker image"
-sudo docker-compose -f docker-compose.dev.yml up
-
-# stop all docker containers
-sudo docker-compose -f docker-compose.dev.yml down
