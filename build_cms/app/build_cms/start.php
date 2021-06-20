42dbead3e6fc4fd69cdfc04fac843ce4fc50eaff
TimDer <timderksen97@icloud.com>
2021-06-20 13:12:49 +0200

    Auto set dir_to_build_cms

--- build_cms/app/build_cms/start.php
+++ build_cms/app/build_cms/start.php
@@ -1,5 +1,9 @@
 <?php
 
+if (!isset($GLOBALS["dir_to_build_cms"])) {
+    $GLOBALS["dir_to_build_cms"] = __DIR__;
+}
+
 $GLOBALS["autoLoadStartDir"] = __DIR__;
 require __DIR__ . "/build_cms_system/autoload.php";
 config::start_cms();
