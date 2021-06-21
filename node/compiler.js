792be282d31916397c6046b039601ea891eb31c1
TimDer <timderksen97@icloud.com>
2021-06-21 17:38:08 +0200

    Remove sys dir

--- node/compiler.js
+++ node/compiler.js
@@ -20,12 +20,11 @@ module.exports = () => {
     // delete dir if it already exists
     if (fs.existsSync(dir.installerDumpDir)) {
         console.log("Deleting existing directory")
-        //delDir.sync(dir.installerDumpDir)
-        delDir.sync(dir.installerDumpDir + "/sys/build_cms/*");
-        delDir.sync(dir.installerDumpDir + "/sys/build_cms/.*");
+        delDir.sync(dir.installerDumpDir + "/build_cms/*");
+        delDir.sync(dir.installerDumpDir + "/build_cms/.*");
 
-        if (fs.existsSync(dir.installerDumpDir + "/sys/.htaccess")) {
-            fs.unlinkSync(dir.installerDumpDir + "/sys/.htaccess");
+        if (fs.existsSync(dir.installerDumpDir + "/.htaccess")) {
+            fs.unlinkSync(dir.installerDumpDir + "/.htaccess");
         }
 
         if (fs.existsSync(dir.installerDumpDir + "/index.php")) {
@@ -36,40 +35,36 @@ module.exports = () => {
         // create the installer dump dir
         console.log("Creating \"installer-dump\" directory")
         fs.mkdirSync(dir.installerDumpDir)
-
-        // create the sys dir
-        console.log("Creating \"sys\" directory")
-        fs.mkdirSync(dir.installerDumpDir + "/sys")
     }
     
-    // copy the cms to the sys dir
-    console.log("Coping the cms to the \"sys\" directory")
-    fse.copySync(dir.sysDir + "/build_cms", dir.installerDumpDir + "/sys/build_cms")
+    // copy the cms to the installer-dump dir
+    console.log("Coping the cms to the \"installer-dump\" directory")
+    fse.copySync(dir.sysDir + "/build_cms", dir.installerDumpDir + "/build_cms")
 
-    // copy the cms backend template the the sys dir
-    console.log("Coping the backend template to the \"sys\" dir")
-    fse.copySync(dir.adminTemplateDir + "/view", dir.installerDumpDir + "/sys/build_cms/build_cms_system/view/admin/admin_basics")
-    fse.copySync(dir.adminTemplateDir + "/www-root", dir.installerDumpDir + "/sys/build_cms/build_cms_system/www-root/admin/admin_basics")
+    // copy the cms backend template the the installer-dump dir
+    console.log("Coping the backend template to the \"installer-dump\" dir")
+    fse.copySync(dir.adminTemplateDir + "/view", dir.installerDumpDir + "/build_cms/build_cms_system/view/admin/admin_basics")
+    fse.copySync(dir.adminTemplateDir + "/www-root", dir.installerDumpDir + "/build_cms/build_cms_system/www-root/admin/admin_basics")
 
-    // copy the user plugins to the sys dir
+    // copy the user plugins to the installer-dump dir
     if (fs.existsSync(dir.pluginDir)) {
         console.log("Coping the user plugins to the plugins directory")
-        fse.copySync(dir.pluginDir, dir.installerDumpDir + "/sys/build_cms/plugins")
+        fse.copySync(dir.pluginDir, dir.installerDumpDir + "/build_cms/plugins")
     }
 
-    // copy the system plugins to the sys dir
+    // copy the system plugins to the installer-dump dir
     if (fs.existsSync(dir.pluginSystemDir)) {
         console.log("Coping the system plugins to the build_cms_system/system directory")
-        fse.copySync(dir.pluginSystemDir, dir.installerDumpDir + "/sys/build_cms/build_cms_system/system")
+        fse.copySync(dir.pluginSystemDir, dir.installerDumpDir + "/build_cms/build_cms_system/system")
     }
     
-    // copy the ".htaccess" file to the sys dir
-    console.log("Coping the htaccess to the \"sys\" directory")
-    fse.copySync(dir.sysDir + "/.htaccess", dir.installerDumpDir + "/sys/.htaccess")
-    
-    // create the installer html
-    console.log("Copying install.php")
-    fse.copySync(dir.installerFile, dir.installerDumpDir + "/index.php")
+    // copy the ".htaccess" file to the installer-dump dir
+    console.log("Coping the htaccess to the \"installer-dump\" directory")
+    fse.copySync(dir.sysDir + "/.htaccess", dir.installerDumpDir + "/.htaccess")
+
+    // copy the "index.compile.php" file to the installer-dump dir
+    console.log("Coping index.compile.php to the \"installer-dump\" directory")
+    fse.copySync(dir.sysDir + "/index.compile.php", dir.installerDumpDir + "/index.php")
 
     // the compiler is done
     console.log("Done")
